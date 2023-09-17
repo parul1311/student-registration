4e6ce0b099f45569f0449fbe3c575c78fd299099
Parul Agrawal <parulsagrawal@gmail.com>
2023-09-18 01:22:54 +0530

    delete unneccesary files and add last updated sql file

--- student_registration.sql
+++ student_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 17, 2023 at 01:40 PM
+-- Generation Time: Sep 17, 2023 at 07:25 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -62,7 +62,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_09_17_134501_laratrust_setup_tables', 2),
+(6, '2023_09_17_143611_add_phone_dob_address_avatar_verified_by_admin_in_users_table', 3);
 
 -- --------------------------------------------------------
 
@@ -77,6 +79,73 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL,
   KEY `password_resets_email_index` (`email`(250))
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `permissions_name_unique` (`name`) USING HASH
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'view_students', 'Manage Students View', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35'),
+(2, 'add_student', 'Manage Student Add', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35'),
+(3, 'edit_student', 'Manage Student Edit', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35'),
+(4, 'delete_student ', 'Manage Student Delete', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission_role`
+--
+
+DROP TABLE IF EXISTS `permission_role`;
+CREATE TABLE IF NOT EXISTS `permission_role` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `role_id` bigint(20) UNSIGNED NOT NULL,
+  PRIMARY KEY (`permission_id`,`role_id`),
+  KEY `permission_role_role_id_foreign` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `permission_role`
+--
+
+INSERT INTO `permission_role` (`permission_id`, `role_id`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission_user`
+--
+
+DROP TABLE IF EXISTS `permission_user`;
+CREATE TABLE IF NOT EXISTS `permission_user` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `user_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`,`permission_id`,`user_type`),
+  KEY `permission_user_permission_id_foreign` (`permission_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -101,6 +170,59 @@ CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_name_unique` (`name`) USING HASH
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'Admin', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35'),
+(2, 'student', 'Student', NULL, '2023-09-17 08:33:35', '2023-09-17 08:33:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_user`
+--
+
+DROP TABLE IF EXISTS `role_user`;
+CREATE TABLE IF NOT EXISTS `role_user` (
+  `role_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `user_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`,`user_type`),
+  KEY `role_user_role_id_foreign` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `role_user`
+--
+
+INSERT INTO `role_user` (`role_id`, `user_id`, `user_type`) VALUES
+(1, 1, 'App\\Models\\User'),
+(2, 2, 'App\\Models\\User'),
+(2, 3, 'App\\Models\\User'),
+(2, 4, 'App\\Models\\User'),
+(2, 5, 'App\\Models\\User'),
+(2, 9, 'App\\Models\\User');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -111,12 +233,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `verify_by_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `address` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`) USING HASH
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `verify_by_admin`, `address`, `dob`, `phone`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@test.com', '2023-09-17 08:33:36', '$2y$10$uKeW7/WfCUVR30PkbWx7E.Bf91qNctoCF6xjs1lgn7nHojBKvBw6i', 0, NULL, NULL, NULL, '', NULL, '2023-09-17 08:33:36', '2023-09-17 08:33:36'),
+(2, 'Student', 'student@test.com', '2023-09-17 08:33:36', '$2y$10$T6mQa88Uv5KM3HG9PcxbZ.loV6qYXGa0qgWn0ivvxhGmxjGNULrbi', 1, 'tset', '2023-08-29', '9874563210', '', NULL, '2023-09-17 08:33:36', '2023-09-17 13:14:55'),
+(3, 'Parul Agrawal', 'paru@gmail.com', NULL, '$2y$10$jdMnqzy9J6DHqhIDbcf/CeaSu1VKqf1mZsstx0O2p6zGWhxtOJrwK', 1, 'test address', '2023-09-06', '9874563210', '0gvLZvQetChSTM6DBUqdM02JwfhlHwMXaJWH0LHI.jpg', NULL, '2023-09-17 08:51:22', '2023-09-17 13:55:09'),
+(4, 'Test User', 'test@gmail.com', NULL, '$2y$10$rv8pgooB9Aat7Wzqz0Lc3.QA/fESWn/G1xcYGKaO224lbA29YO.hG', 1, NULL, NULL, NULL, 'wDWS3zIWwTAqTtlY1LyKyYw6aJcejPD8Y1yGo4Yw.jpg', NULL, '2023-09-17 10:09:22', '2023-09-17 13:15:05'),
+(5, 'Parul Agrawal', 'pa@pa.com', NULL, '$2y$10$XwDsZ16.OqrgAHXHiDZ9a.rUNMQpXoG5Zed3oJlxRbt6dgIzSZV9q', 1, 'tests', '2023-09-09', '9874521036', NULL, NULL, '2023-09-17 12:17:29', '2023-09-17 13:15:36'),
+(9, 'demo test', 'demo2@test.com', NULL, '$2y$10$iwZRYGNb1fGVw4c1s23vLupv4ae2k79RK0sB/6l9kvC5ppzMwvr2K', 0, NULL, NULL, NULL, '2NUfn6SqlCdfgeDyYR6KJEIrWescbrf7ncxDla9H.png', NULL, '2023-09-17 12:40:53', '2023-09-17 12:40:53');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
